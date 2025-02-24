5ad475a851e0f9322f66ac95cf681f0ff125a8d5
LEONARDO PEREIRA DE OLIVEIRA <115956767+LPvaultHunter@users.noreply.github.com>
2025-02-24 19:34:24 -0300

    1 o resultado é carta1, 0 o resultado é carta2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,7 @@
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
 
 int main() {
     printf("Desafio Super Trunfo - Países. \n");
@@ -27,8 +27,8 @@ int main() {
     float PIB;
     float PIBP;
     float area;
-    float densidade;
-    int Populacao;
+    float densidade, SuperPoder;
+    unsigned long int Populacao;
     int pontos;
 
     // Cadastro das Cartas:
@@ -36,13 +36,13 @@ int main() {
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     printf("Digite um Estado de A até H: \n");
-    scanf("%s", &Estado);
+    scanf("%s", Estado);
 
     printf("Digite o Código da carta: \n");
-    scanf("%s", &Codigo);
+    scanf("%s", Codigo);
 
     printf("Digite o Nome da cidade: \n");
-    scanf("%s", &Nome);
+    scanf("%s", Nome);
 
     printf("PIB da Cidade: \n");
     scanf("%f", &PIB);
@@ -57,9 +57,10 @@ int main() {
     scanf("%d", &pontos);
    
 // adicionando pib per capita e densidade populacional, resultado sera exibido juntamente com os resultado das cartas
+// Super Poder
     PIBP = (float)(PIB / Populacao);
     densidade = (float)(Populacao /area);
-
+    SuperPoder = (Populacao + area + PIB + pontos + PIBP +(1 / densidade));
     
     printf("\n");
 
@@ -71,8 +72,8 @@ int main() {
     float PIBB1;
     float PIBPB1;
     float areaB1;
-    float densidadeB1;
-    int PopulacaoB1;
+    float densidadeB1, SuperPoderB1;
+    unsigned long int PopulacaoB1;
     int pontosB1;
 
     // Cadastro das Cartas:
@@ -80,13 +81,13 @@ int main() {
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     printf("Digite um Estado de A até H: \n");
-    scanf("%s", &EstadoB1);
+    scanf("%s", EstadoB1);
 
     printf("Digite o Código da carta: \n");
-    scanf("%s", &CodigoB1);
+    scanf("%s", CodigoB1);
 
     printf("Digite o Nome da cidade: \n");
-    scanf("%s", &NomeB1);
+    scanf("%s", NomeB1);
 
     printf("PIB da Cidade: \n");
     scanf("%f", &PIBB1);
@@ -102,8 +103,10 @@ int main() {
 
     PIBPB1 = (float)(PIBB1 / PopulacaoB1);
     densidadeB1 = (float)(PopulacaoB1 / areaB1);
+    SuperPoderB1 = (PopulacaoB1 + areaB1 + PIBB1 + pontosB1 + PIBPB1 +(1 / densidadeB1));
     // Exibição dos Dados de ambas as Cartas:
     // Exibindo os valores inseridos para cada atributo da cidade, um por linha.
+    //superpoder adicionado
 
     
     
@@ -113,7 +116,7 @@ int main() {
     
     Printf("\n");
 
-      printf("Estado: %s\n", &Estado);
+    printf("Estado: %s\n", &Estado);
     printf("Codigo: %s\n", &Codigo);
     printf("Nome: %s\n", &Nome);
     printf("PIB: %2.f $\n", PIB);
@@ -142,6 +145,19 @@ int main() {
     printf("\n");
 
     // RESULTADOS DAS CARTAS, COM A NOVA FUNÇÃO DE DENSIDADE E PIB PER CAPITA****
+    //as comparações logo abaixo
+
+    printf(" Comparacao de Cartas \n\n");
+  
+    printf("Populacao: %d\n", populacao > populacaoB1 );
+    printf("Area da Cidade: %d\n", Area > areaB1);
+    printf("Densidade populacional: %d\n", densidade < densidadeB1);
+    printf("PIB da Cidade: %d\n", PIB > PIBB1);
+    printf("PIB per capita: %d\n", PIBP > PIBPB1);
+    printf("Pontos Turiticos: %d\n", pontos > pontosB1);
+    printf("SuperPoder: %d\n", SuperPoder > SuperPoderB1);
+
+    //1 o resultado é carta1, 0 o resultado é carta2***
 
     return 0;    
 }
